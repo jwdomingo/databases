2b9735d160cb2b50ef2c498706ee047777175326
John Domingo <johnwill.domingo@gmail.com>
2016-02-05 23:54:16 -0800

    Temporarily modify table schema for spec-runner tests to pass

--- server/schema.sql
+++ server/schema.sql
@@ -3,24 +3,24 @@ USE chat;
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(20) NOT NULL,
+  username VARCHAR(20) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE rooms (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(20) NOT NULL,
+  roomname VARCHAR(20) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE messages (
   id INT NOT NULL AUTO_INCREMENT,
-  user INT NOT NULL,
-  room INT NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  roomname VARCHAR(255) NOT NULL,
   message VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id),
+  PRIMARY KEY (id)/*,
   FOREIGN KEY (id) REFERENCES users(id),
-  FOREIGN KEY (id) REFERENCES rooms(id)
+  FOREIGN KEY (id) REFERENCES rooms(id)*/
 );
 
 /*  Execute this file from the command line by typing:
