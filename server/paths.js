1ba777c352a5b4afb08e1e38ac64b113ce167e45
John Domingo <johnwill.domingo@gmail.com>
2016-02-05 22:30:18 -0800

    Add two spaces instead of tab

--- server/paths.js
+++ server/paths.js
@@ -2,5 +2,5 @@ var path = require('path');
 
 module.exports = {
 	index: path.join(__dirname, '../client/index.html'),
-  public: path.join(__dirname, '../client')
+	public: path.join(__dirname, '../client')
 };
