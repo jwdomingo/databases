b6468f9ca1bf862f83b4945cc601e3af743c3d09
John Domingo <johnwill.domingo@gmail.com>
2016-02-06 11:49:30 -0800

    Implement models.messages.get to handle database interfacing

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,18 @@
-// var db = require('../db');
+var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      console.log('MODELS RECIEVES RE#UEST');
+      var sql = 'SELECT * FROM messages';
+      return db.connection.query(sql, function(err, rows, fields) {
+        if (err) { throw err; }
+        callback(err, rows);
+      });
+    },
+    post: function () {
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
@@ -12,4 +21,3 @@ module.exports = {
     post: function () {}
   }
 };
-
