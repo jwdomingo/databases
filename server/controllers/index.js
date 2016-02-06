ea65b332c1ed86231479f1aab421c19c5660dfbd
jwdomingo <johnwill.domingo@gmail.com>
2016-02-05 17:34:36 -0800

    require db module and start connection

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,39 @@
 var models = require('../models');
+var mysql = require('mysql');
+var db = require('./../db/index.js');
+
+db.connection.connect();
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+
+    }, // a function which handles a get request for all messages
+    
+    post: function (req, res) {
+
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+
+    },
+
+    post: function (req, res) {
+
+      mysql.query();
+
+      // db.
+
+    // connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+ 
+//   console.log('The solution is: ', rows[0].solution);
+// });
+
+
+    }
   }
 };
-
