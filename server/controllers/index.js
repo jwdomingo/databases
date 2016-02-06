3181018f64a510f7558f236739ad97e4efc77f3c
John Domingo <johnwill.domingo@gmail.com>
2016-02-05 23:52:53 -0800

    Implement post users and post messages methods

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,11 +7,19 @@ db.connection.connect();
 module.exports = {
   messages: {
     get: function (req, res) {
-
+      // var sql = 'SELECT * FROM messages';
+      // db.connection.query(sql, function(err, rows, fields) {
+      //   if (err) { throw error; };
+      // });
     }, // a function which handles a get request for all messages
-    
-    post: function (req, res) {
 
+    post: function (req, res) {
+      var sql = 'INSERT INTO messages SET ?'
+      db.connection.query(sql, req.body, function(err, rows, fields) {
+        if (err) { throw error; };
+        console.log('MESSAGE posted with insertID:', rows.insertId);
+        res.end();
+      });
     } // a function which handles posting a message to the database
   },
 
@@ -22,9 +30,12 @@ module.exports = {
     },
 
     post: function (req, res) {
-
-      // mysql.query();
-
+      var sql = 'INSERT INTO users SET ?'
+      db.connection.query(sql, req.body, function(err, rows, fields) {
+        if (err) { throw error; };
+        console.log('USER posted with insertID:', rows.insertId);
+        res.end();
+      });
     }
   }
 };
